2bf1614356d1c17f397a6b52c5ff61ec288fdca0
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-09 15:17:14 +0100

    Creating a new Hero and displaying it on Appcomponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes', // the component's CSS element selector
+  templateUrl: './heroes.component.html', // the location of the component's template file.
+  styleUrls: ['./heroes.component.css'] //  the location of the component's private CSS styles.
 })
+
+// Always export the component class so you can import it elsewhere ... like in the AppModule.
 export class HeroesComponent implements OnInit {
+  hero = 'Windstorm';
 
   constructor() { }
 

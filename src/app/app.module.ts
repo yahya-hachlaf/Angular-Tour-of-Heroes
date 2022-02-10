5be0a408b7b8531998a6f12ee2d7d19abe82e3ac
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-10 15:35:20 +0100

    splitting the heroesComponent to 2 Components: (Parent) heroesComponent and (Child) heroDetailComponent . the functionality stays the same just the responsability has been divided.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; // <-- NgModel lives here
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
 
 @NgModule({
   declarations: [

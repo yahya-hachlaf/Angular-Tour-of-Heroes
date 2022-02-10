84e554a9dc04a14dcacac6906d3fc614fd5a4aaf
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-10 15:57:58 +0100

    importing hero and heroes and creating a getter of the mock heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,9 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  // return the mock heroes
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }

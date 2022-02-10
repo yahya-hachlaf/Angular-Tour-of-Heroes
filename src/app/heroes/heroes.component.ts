0edd3aa59680ccb6f1cfe151a5b129aa94e15516
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-10 14:43:26 +0100

    Displaying the selected hero's details and making the clicked button highlighted

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,6 +18,12 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  //  assign the clicked hero from the template to the component's selectedHero
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit(): void {

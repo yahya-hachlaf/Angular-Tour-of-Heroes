c43ebc70fc26be3a07991a7d3cc002d38f29431b
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-11 12:16:08 +0100

    Injecting MessageService into HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// this is a service-in-service scenario where the MessageService
+// is injected into the HeroService which is injected into the HeroesComponent too.
 export class HeroService {
 
-  constructor() { }
+  // Angular will inject the singleton MessageService 
+  // into that property when it creates the HeroService.
+  constructor(private messageService: MessageService) { }
 
   // return the mock heroes
   getHeroes(): Observable<Hero[]> {

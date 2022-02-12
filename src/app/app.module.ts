0ca233a9b8bb274c0df3604db6dc9b4f493fc400
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-12 22:45:54 +0100

    adding som styling to messageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core'; // <-- NgModel lives here
 import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core'; // <-- NgModel lives here
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component'; 
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({

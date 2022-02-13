f34666af9358d63b94684f85af9836ad232783fb
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-13 19:14:02 +0100

    configurating routes to tell router to display heroes when a user clicks on the corresponded link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'; // so the application can have routing functionality.
+import { HeroesComponent } from './heroes/heroes.component'; // will give the Router somewhere to go once you configure the routes.
 
+//Routes tell the Router which view to display when a 
+//user clicks a link or pastes a URL into the browser address bar.
+const routes: Routes = [
+  { path: 'heroes', //  a string that matches the URL in the browser address bar.
+    component: HeroesComponent // the component that the router should create when navigating to this route.
+  }
+];
 
-
+// add initializes the router and starts it listening for browser location changes.
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)], // adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step by calling RouterModule.forRoot()
+  exports: [RouterModule] // AppRoutingModule exports RouterModule so it will be available throughout the application.
 })
 export class AppRoutingModule { }

90e7095ec29b93ac72bc94ef705dc923aa64b7b5
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-14 11:53:48 +0100

    Add a hero detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // so the application can have routing functionality.
 import { HeroesComponent } from './heroes/heroes.component'; // will give the Router somewhere to go once you configure the routes.
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 //Routes tell the Router which view to display when a 
 //user clicks a link or pastes a URL into the browser address bar.
@@ -10,7 +11,8 @@ const routes: Routes = [
     component: HeroesComponent // the component that the router should create when navigating to this route.
   },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' } // To make the application navigate to the dashboard automatically 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // To make the application navigate to the dashboard automatically 
+  { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 // add initializes the router and starts it listening for browser location changes.

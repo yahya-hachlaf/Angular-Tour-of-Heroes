351edfd2c104d8acd80eefc5e74d3f534fc87f44
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-14 10:33:12 +0100

    Add the dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // so the application can have routing functionality.
 import { HeroesComponent } from './heroes/heroes.component'; // will give the Router somewhere to go once you configure the routes.
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 //Routes tell the Router which view to display when a 
 //user clicks a link or pastes a URL into the browser address bar.
 const routes: Routes = [
   { path: 'heroes', //  a string that matches the URL in the browser address bar.
     component: HeroesComponent // the component that the router should create when navigating to this route.
-  }
+  },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 // add initializes the router and starts it listening for browser location changes.

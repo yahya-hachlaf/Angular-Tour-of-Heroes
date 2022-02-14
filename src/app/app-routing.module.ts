3593241f214384542187258a8408cd119804378f
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-14 13:28:59 +0100

    delete unnecessary comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // so the application can have routing functionality.
+
 import { HeroesComponent } from './heroes/heroes.component'; // will give the Router somewhere to go once you configure the routes.
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -7,9 +8,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 //Routes tell the Router which view to display when a 
 //user clicks a link or pastes a URL into the browser address bar.
 const routes: Routes = [
-  { path: 'heroes', //  a string that matches the URL in the browser address bar.
-    component: HeroesComponent // the component that the router should create when navigating to this route.
-  },
+  { path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // To make the application navigate to the dashboard automatically 
   { path: 'detail/:id', component: HeroDetailComponent }

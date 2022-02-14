e41055c6667cb7f32bb670b02844c456bf85251b
Yahya-Hachlaf <yahya.hachlaf@bechtle.com>
2022-02-14 12:02:02 +0100

    Get the route that created the hero so it will be displaed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,7 +14,11 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero; // Input decorator make the hero propety available for binding by the external HeroesComponent
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, // holds information about the route to this instance of the HeroDetailComponent. 
+    private heroService: HeroService, // gets hero data from the remote server and this component will use it to get the hero-to-display.
+    private location: Location // is an Angular service for interacting with the browser.
+  ) {}
 
   ngOnInit(): void {
   }
